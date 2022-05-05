babdf8123c51a4e57014f6448ad35e86bbe0da6e
Trang Lam <ms.tranglam@gmail.com>
2022-05-04 22:50:36 -0700

    changes UI

--- TicTacToe/GameBoardViewController.swift
+++ TicTacToe/GameBoardViewController.swift
@@ -13,13 +13,15 @@ class GameBoardViewController: UIViewController {
     var player2 = ""
     var size = 0.0
     var xMove = 1
+    var playerMove = Array<Array<String>>()
+    var mappingTagToCoordinate = [Int: (Int, Int)]()
     @IBOutlet weak var gameBoardView: UIView!
     @IBOutlet weak var anounceLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        anounceLabel.text = "\(player1) turn!"
-        gameBoardView.backgroundColor = .black
+        anounceLabel.text = "\(player1)'s turn!"
+        gameBoardView.backgroundColor = .white
         view.addSubview(gameBoardView)
         createUI()
 
@@ -29,19 +31,23 @@ class GameBoardViewController: UIViewController {
     func createUI() {
         let buffer = 3.0
         let totalW = gameBoardView.frame.width - (buffer * (size - 1))
-        let btnW = totalW / size
+        let btnSide = totalW / size
         var tag = 0
         for col in 0..<Int(size) {
             let c = Double(col)
-            let b = (btnW * c) + (buffer * c)
+            let y = (btnSide * c) + (buffer * c)
+            var tempArray = Array<String>()
             for r in 0..<Int(size) {
                 let j = Double(r)
-                let x = (btnW * j) + (buffer * j)
-                let btn = makeBtn(x: x, buffer: b, btnW: btnW)
+                let x = (btnSide * j) + (buffer * j)
+                let btn = makeBtn(x: x, y: y, side: btnSide)
                 btn.tag = tag
+                mappingTagToCoordinate[tag] = (r, col)
                 tag += 1
                 btn.setTitle("", for: .normal)
+                tempArray.append("nil")
             }
+            playerMove.append(tempArray)
         }
     }
     
@@ -49,36 +55,38 @@ class GameBoardViewController: UIViewController {
         return [UIButton]()
     }
     
-    func makeBtn(x : CGFloat, buffer: CGFloat, btnW: CGFloat) -> UIButton {
-        let r = CGRect(x: x, y: buffer, width: btnW, height: btnW)
+    func makeBtn(x : CGFloat, y: CGFloat, side: CGFloat) -> UIButton {
+        let r = CGRect(x: x, y: y, width: side, height: side)
         let btn = UIButton(type: .system)
         btn.frame = r
-        btn.backgroundColor = .white
+        btn.backgroundColor = .black
         
         gameBoardView.addSubview(btn)
         
-        
-        btn.addTarget(self,
-                      action: #selector(doGameBtn),
-                      for: .touchUpInside)
-        print("btn")
+//        
+//        btn.addTarget(self,
+////                      action: #selector(doGameBtn),
+//                      for: .touchUpInside)
         return btn
     }
-    @objc func doGameBtn(sender : UIButton) {
-//        let btnTag = sender.tag
-        sender.isEnabled = false
-        if xMove > 0 {
-            sender.setTitle("X", for: .normal)
-            sender.setTitleColor(.blue, for: .normal)
-            sender.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-            anounceLabel.text = "\(player2) turn!"
-        } else {
-            sender.setTitle("O", for: .normal)
-            sender.setTitleColor(.red, for: .normal)
-            sender.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-            anounceLabel.text = "\(player1) turn!"
-        }
-        xMove *= -1
-    }
+    
+//    @objc func doGameBtn(sender : UIButton) {
+//        sender.isEnabled = false
+//        let coor = (mappingTagToCoordinate[sender.tag][0], mappingTagToCoordinate[sender.tag][1])
+//        if xMove > 0 {
+//            sender.setTitle("X", for: .normal)
+//            sender.setTitleColor(.blue, for: .normal)
+//            sender.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+//            anounceLabel.text = "\(player2) turn!"
+//            playerMove[coor[0], coor[1]] = "X"
+//        } else {
+//            sender.setTitle("O", for: .normal)
+//            sender.setTitleColor(.red, for: .normal)
+//            sender.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+//            anounceLabel.text = "\(player1) turn!"
+//            playerMove[coor[0], coor[1]] = "X"
+//        }
+//        xMove *= -1
+//    }
 
 }
--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -15,20 +15,73 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var player1Label: UILabel!
     @IBOutlet weak var player2Label: UILabel!
     @IBOutlet weak var sizeLabel: UILabel!
+    @IBOutlet weak var startBtn: UIButton!
+    @IBOutlet weak var player1VS: UILabel!
+    @IBOutlet weak var player2VS: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
     
     var sizeInInt = 3
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        titleSetting()
+        uiSetting()
+
     }
+    
+    func uiSetting(){
+        player1TF.layer.borderColor = UIColor.white.cgColor
+        player1TF.layer.borderWidth = 3.0
+        player1TF.layer.cornerRadius = 10
+        player1TF.attributedPlaceholder = NSAttributedString(
+            string: "Player 1",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray]
+        )
+        
+        player2TF.layer.borderColor = UIColor.white.cgColor
+        player2TF.layer.borderWidth = 3.0
+        player2TF.layer.cornerRadius = 10
+        player2TF.attributedPlaceholder = NSAttributedString(
+            string: "Player 2",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray])
+        
+        sizeTF.layer.borderColor = UIColor.white.cgColor
+        sizeTF.layer.borderWidth = 3.0
+        sizeTF.layer.cornerRadius = 10
+        sizeTF.attributedPlaceholder = NSAttributedString(
+            string: "Size",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray])
+        
+        startBtn.layer.borderColor = UIColor.white.cgColor
+        startBtn.layer.borderWidth = 3.0
+        startBtn.layer.cornerRadius = 10
+    }
+    
+    func titleSetting(){
+        
+        //set amination
+        let titleText = "Tic Tac Toe"
+        titleLabel.text = ""
+       var charIndex = 0
+       for char in titleText{
+           Timer.scheduledTimer(withTimeInterval: 0.2 * Double(charIndex), repeats: false) { _ in
+               self.titleLabel.text?.append(char)
+           }
+           charIndex += 1
+       }
+    }
+    
 
     @IBAction func player1Name(_ sender: UITextField) {
         player1Label.text = sender.text
+        player1VS.text = sender.text
     }
     
     @IBAction func player2Name(_ sender: Any) {
         player2Label.text = player2TF.text
+        player2VS.text = player2TF.text
     }
+    
     
     @IBAction func gameSize(_ sender: Any) {
         guard let size = sizeTF.text else {return}
@@ -46,7 +99,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         player1TF.text = ""
         player2TF.text = ""
         sizeTF.text = ""
-        
+
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
